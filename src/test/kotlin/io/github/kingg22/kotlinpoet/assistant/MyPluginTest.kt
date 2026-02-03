4a88cadbf2073a1ee88b3d2fcd9235418f8ddc43
Rey <89825425+kingg22@users.noreply.github.com>
2026-02-03 10:35:47 -0500

    misc: remove template code, maintain the test for future usage

--- src/test/kotlin/io/github/kingg22/kotlinpoet/assistant/MyPluginTest.kt
+++ src/test/kotlin/io/github/kingg22/kotlinpoet/assistant/MyPluginTest.kt
@@ -1,12 +1,10 @@
-package org.jetbrains.plugins.template
+package io.github.kingg22.kotlinpoet.assistant
 
 import com.intellij.ide.highlighter.XmlFileType
-import com.intellij.openapi.components.service
 import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import org.jetbrains.plugins.template.services.MyProjectService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
@@ -29,11 +27,5 @@ class MyPluginTest : BasePlatformTestCase() {
         myFixture.testRename("foo.xml", "foo_after.xml", "a2")
     }
 
-    fun testProjectService() {
-        val projectService = project.service<MyProjectService>()
-
-        assertNotSame(projectService.getRandomNumber(), projectService.getRandomNumber())
-    }
-
-    override fun getTestDataPath() = "src/test/testData/rename"
+    override fun getTestDataPath(): String = "src/test/testData/rename"
 }
