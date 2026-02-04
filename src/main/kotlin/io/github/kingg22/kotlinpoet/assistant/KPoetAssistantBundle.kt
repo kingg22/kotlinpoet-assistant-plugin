ef5712a3ece449e8a7ac13e9560bfbadca10b818
Rey <89825425+kingg22@users.noreply.github.com>
2026-02-03 22:36:43 -0500

    perf: remove unused code and rename to KPoetAssistantBundle

---
+++ src/main/kotlin/io/github/kingg22/kotlinpoet/assistant/KPoetAssistantBundle.kt
@@ -0,0 +1,5 @@
+package io.github.kingg22.kotlinpoet.assistant
+
+import com.intellij.DynamicBundle
+
+object KPoetAssistantBundle : DynamicBundle(KPoetAssistantBundle::class.java, "messages.MyBundle")
