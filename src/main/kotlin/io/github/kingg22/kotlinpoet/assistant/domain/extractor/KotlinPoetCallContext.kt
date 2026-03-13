d8949d7421d6ca38c5db4a3f917fc529a928ae3f
Rey <89825425+kingg22@users.noreply.github.com>
2026-03-12 20:01:20 -0500

    feat: add RenderHint and update KotlinPoetCallContext
    This prepared data for preview code

--- src/main/kotlin/io/github/kingg22/kotlinpoet/assistant/domain/extractor/KotlinPoetCallContext.kt
+++ src/main/kotlin/io/github/kingg22/kotlinpoet/assistant/domain/extractor/KotlinPoetCallContext.kt
@@ -3,4 +3,8 @@ package io.github.kingg22.kotlinpoet.assistant.domain.extractor
 import io.github.kingg22.kotlinpoet.assistant.domain.model.ArgumentSource
 import io.github.kingg22.kotlinpoet.assistant.domain.model.FormatStringModel
 
-data class KotlinPoetCallContext(val format: FormatStringModel, val arguments: ArgumentSource)
+data class KotlinPoetCallContext(
+    val format: FormatStringModel,
+    val arguments: ArgumentSource,
+    val renderHint: RenderHint? = null,
+)
---
+++ src/main/kotlin/io/github/kingg22/kotlinpoet/assistant/domain/extractor/RenderHint.kt
@@ -0,0 +1,3 @@
+package io.github.kingg22.kotlinpoet.assistant.domain.extractor
+
+data class RenderHint(val methodName: String, val receiverFqName: String?, val isDelegated: Boolean)
